5b2cd7e37855cc4d8364b0a7841fd22b883f9e2f
MBrian Website <muhammad.brian.27@gmail.com>
2023-03-01 14:54:16 +0700

    IMPORT REPORT MODULE TO SUMMARY MODULE

--- src/summary/summary.service.ts
+++ src/summary/summary.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Get } from '@nestjs/common';
+import { ReportService } from 'src/report/report.service';
 
 @Injectable()
-export class SummaryService {}
+export class SummaryService {
+  constructor(private readonly reportService: ReportService) {}
+
+  @Get()
+  calculateSummary() {
+    return;
+  }
+}
